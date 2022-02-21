f7de02552d653a9dd8dfc02a4aac0daacff8640b
Ricardo <l42647@alunos.uevora.pt>
2022-02-21 09:25:22 +0000

    deploy: App module and components

--- projects/calculator/src/app/app.module.ts
+++ projects/calculator/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculatorModule } from './calculator';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalculatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
