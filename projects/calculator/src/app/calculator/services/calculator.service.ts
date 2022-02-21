5046c9592e694521d0a3f7d4db66b837dd886904
Ricardo <l42647@alunos.uevora.pt>
2022-02-21 10:48:49 +0000

    add: Operations
    Update the services in order to add a method that computes the operations results. Few comments were also added

--- projects/calculator/src/app/calculator/services/calculator.service.ts
+++ projects/calculator/src/app/calculator/services/calculator.service.ts
@@ -1,3 +1,11 @@
+/**
+ * Service responsible to execute all calculator
+ * operations. 
+ * 
+ * @author Ricardo Oliveira<ricardooliveira@optiply.nl>
+ * @since v1.0
+ */
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +13,52 @@ import { Injectable } from '@angular/core';
 })
 export class CalculatorService {
 
+  static readonly ADDITION: string = "+";
+  static readonly SUBTRACTION: string = "-";
+  static readonly MULTIPLICATION: string = "*";
+  static readonly DIVISION: string = "/";
+
   constructor() { }
+
+  /**
+   * Method that computes an arithmetic operation
+   * given two numbers. Supports addition, subtraction,
+   * multiplication and division.
+   * 
+   * @param num1 number
+   * @param num2 number
+   * @param operation string Operation to be executed
+   * @returns number Operation result
+   */
+  compute(num1: number, num2: number, operation: string): number { 
+
+    let result: number; // stores the operation result
+
+    switch(operation) {
+
+      case CalculatorService.ADDITION:
+        result = num1 + num2;
+        break;
+
+      case CalculatorService.SUBTRACTION:
+        result = num1 - num2;
+        break;
+
+      case CalculatorService.MULTIPLICATION:
+        result = num1 * num2;
+        break;
+
+      case CalculatorService.DIVISION:
+        result = num1 / num2;
+        break;
+
+      default:
+        result = 0;
+
+    }
+
+    return result;
+
+  }
+
 }
