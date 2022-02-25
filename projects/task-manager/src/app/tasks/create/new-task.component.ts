a941df3d2f7fee4fcdb974df2409701f76a0c3b3
Ricardo <l42647@alunos.uevora.pt>
2022-02-25 23:34:23 +0000

    update: Create new task intarface
    Finish the interface for the new task creation page

--- projects/task-manager/src/app/tasks/create/new-task.component.ts
+++ projects/task-manager/src/app/tasks/create/new-task.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { TaskService, Task } from '../shared'; 
 
 @Component({
   selector: 'app-new-task',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewTaskComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('taskForm', { static: true }) taskForm: NgForm;
+  task: Task;
+
+  constructor(private taskService: TaskService, private router: Router) { }
 
   ngOnInit(): void {
+    this.task = new Task();
+  }
+
+  create(): void {
+
+    if (this.taskForm.form.valid) {
+      this.taskService.createTask(this.task);
+      this.router.navigate(["/tarefas"]);
+    }
+    
   }
 
 }
