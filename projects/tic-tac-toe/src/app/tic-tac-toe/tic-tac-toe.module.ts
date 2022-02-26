3e5c14a663f9b33983fb28e7046296bde3c482ad
Ricardo <l42647@alunos.uevora.pt>
2022-02-26 15:55:31 +0000

    deploy: Tic Tac Toe component
    Add the game component and also change the app component html to use the app tag

--- projects/tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.module.ts
+++ projects/tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TicTacToeComponent } from './tic-tac-toe.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TicTacToeComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    TicTacToeComponent
   ]
 })
 export class TicTacToeModule { }
