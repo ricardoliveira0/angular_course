888d3c26026612554783bb399020ec7dfddf82ec
Ricardo <l42647@alunos.uevora.pt>
2022-02-28 01:33:01 +0000

    update: UI
    Added final methods needed and finish UI (control page dynamically)

--- projects/tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ projects/tic-tac-toe/src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TicTacToeService } from './shared';
+
 @Component({
   selector: 'app-tic-tac-toe',
   templateUrl: './tic-tac-toe.component.html',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicTacToeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gameService: TicTacToeService) { }
 
   ngOnInit(): void {
+    this.gameService.gameStartup();
+  }
+
+  get displayStart(): boolean {
+    return this.gameService.showStart;
+  }
+
+  get displayMatrix(): boolean {
+    return this.gameService.showMatrix;
+  }
+
+  get displayFinal(): boolean {
+    return this.gameService.showFinal;
+  }
+
+  startGame(): void {
+    this.gameService.startGame();
+  }
+
+  play(posX: number, posY: number): void {
+    this.gameService.play(posX, posY);
+  }
+
+  displayX(posX: number, posY: number): boolean {
+    return this.gameService.displayX(posX, posY);
+  }
+
+  displayO(posX: number, posY: number): boolean {
+    return this.gameService.displayO(posX, posY);
+  }
+
+  displayWin(posX: number, posY: number): boolean {
+    return this.gameService.displayWin(posX, posY);
+  }
+
+  get player(): number {
+    return this.gameService.player;
+  }
+
+  newGame(): void {
+    this.gameService.newGame();
   }
 
 }
