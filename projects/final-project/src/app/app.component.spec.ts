830c61c24a6f69a22b4ffb1cc066d0dbfd8e5e6a
Ricardo <l42647@alunos.uevora.pt>
2022-03-03 15:03:39 +0000

    remove: Default unit test

--- projects/final-project/src/app/app.component.spec.ts
+++ projects/final-project/src/app/app.component.spec.ts
@@ -16,16 +16,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'final-project'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('final-project');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('final-project app is running!');
-  });
 });
